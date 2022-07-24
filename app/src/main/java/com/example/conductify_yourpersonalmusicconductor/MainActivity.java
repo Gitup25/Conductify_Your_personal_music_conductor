bd58a823345a837d6e7244e1ae2e340887cb29c0
Gitup25 <malama.h.malama@gmail.com>
2022-07-24 10:01:27 +0200

    Initial commit

---
+++ app/src/main/java/com/example/conductify_yourpersonalmusicconductor/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.conductify_yourpersonalmusicconductor;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
